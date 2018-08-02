5363d3ed66000a250a1a79e145ad88aef3a6c9ad
namki <rltmd1004@naver.com>
2018-08-02 17:44:39 +0900

    0802 python

---
+++ python/question_python(Unresolved)/chapter10_test_and_debug/x_all_same.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+def is_all_the_same(input_list):
+    """ 입력된 리스트의 요소들이 모두 같은 값이면 True를, 아니면 False를 반환하는 함수를 작성하자
+    """
+    # 여기 작성
+    return
+
+
+class TestCase(unittest.TestCase):
+    """ is_all_the_same 함수를 테스트하는 코드를 작성하자
+    """
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ python/question_python(Unresolved)/chapter10_test_and_debug/x_even_odd.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+def even_odd(input_list):
+    """ 입력된 정수 리스트에서 짝수와 홀수 개수를 세어 튜플 형태로 반환하는 함수를 작성하자
+    """
+    cnt_even = cnt_odd = 0
+    # 여기 작성
+    return cnt_even, cnt_odd
+
+
+class TestCase(unittest.TestCase):
+    """  even_odd 함수를 테스트하는 코드를 작성하자
+    """
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
