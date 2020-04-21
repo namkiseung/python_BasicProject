d61ef69834e997ea9b66012fe6083cbe19109d49
namkiseung <rltmd1004@naver.com>
2020-04-21 05:31:31 +0000

    add requirement and modify code of mobile browser

--- nmap_script_puzzying/Check_serviceOfIP/Check_ServiceOfPage.py
+++ nmap_script_puzzying/Check_serviceOfIP/Check_ServiceOfPage.py
@@ -8,7 +8,11 @@ from selenium.webdriver.support.wait import WebDriverWait
 options = Options()
 options.headless = False#True
 options.add_argument('--ignore-certificate-errors')
-
+'''
+options.add_argument('--ignore-certificate-errors')
+mobile_emulation = {"deviceMetrics": { "width": 360, "height": 640, "pixelRatio": 3.0 },"userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19" }
+options.add_experimental_option("mobileEmulation", mobile_emulation)
+'''
 wb = Workbook()
 ws = wb.active
 ws.cell(row=1, column=1).value = "No."
@@ -36,6 +40,7 @@ def accessURL(index):
                 time.sleep(1)
                 driver.implicitly_wait(2)
                 driver.get('{}://{}'.format(http_protocol,s))
+                #driver = webdriver.Remote(command_executor='{}://{}'.format(http_protocol,s),desired_capabilities = chrome_options.to_capabilities())
                 html = driver.page_source # 페이지의 elements모두 가져오기
                 print("총 {}중 {}번 완료".format('195',index))
                 #print(s, end='')
